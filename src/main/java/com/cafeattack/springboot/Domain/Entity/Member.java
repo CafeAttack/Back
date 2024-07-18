ed6b9e3baa674bf3709cc768bd6184e1043af4a7
seyeon22 <sywolleych@gmail.com>
2024-07-18 18:16:00 +0900

    기본 코드

--- src/main/java/com/cafeattack/springboot/Domain/Entity/Member.java
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/Member.java
@@ -1,4 +1,31 @@
 package com.cafeattack.springboot.Domain.Entity;
 
+import com.cafeattack.springboot.Exception.BadRequestException;
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int member_id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Date birth;
 }
--- src/main/java/com/cafeattack/springboot/common/BaseResponse.java
+++ src/main/java/com/cafeattack/springboot/common/BaseResponse.java
@@ -1,4 +1,30 @@
 package com.cafeattack.springboot.common;
 
-public class BaseResponse {
+import lombok.Getter;
+
+@Getter
+public class BaseResponse<T> {
+
+    private final int status;
+    private String message;  // 응답에 메시지 포함
+    private T data;
+
+    // status, message, data 모두 넘겨주는 response
+    public BaseResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    // status, message 만 넘겨주는 response
+    public BaseResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    // status, data 만 넘겨주는 response
+    public BaseResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
 }
