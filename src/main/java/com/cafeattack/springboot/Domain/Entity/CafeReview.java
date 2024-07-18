5734e9587262a075c15e4c8e3408000a1a6b7919
Aqoom01 <rlagusgh789@daum.net>
2024-07-18 20:04:00 +0900

    emtity set

--- src/main/java/com/cafeattack/springboot/Domain/Entity/CafeReview.java
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/CafeReview.java
@@ -1,4 +1,18 @@
 package com.cafeattack.springboot.Domain.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class CafeReview {
+
+    @EmbeddedId
+    private CafeReviewPK id;
+
+    public CafeReview(CafeReviewPK id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/cafeattack/springboot/Domain/Entity/Group.java
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/Group.java
@@ -1,4 +1,37 @@
 package com.cafeattack.springboot.Domain.Entity;
 
+
+import com.cafeattack.springboot.Domain.Dto.request.GroupRequestDto;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@NoArgsConstructor
+@Entity
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int group_id;
+
+    @Column(nullable = false)
+    private String group_name;
+
+    @ElementCollection
+    @CollectionTable(name = "group_cafe_ids", joinColumns = @JoinColumn(name = "group_id"))
+    @Column(name = "cafe_id")
+    private List<Integer> cafeIds = new ArrayList<>();
+
+    public Group(String group_name) {
+        this.group_name = group_name;
+    }
+
+    public Group(GroupRequestDto dto) {
+        this.group_name = dto.getGroupName();
+    }
+
 }
