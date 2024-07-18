69e21a8157e3c69c76528064f62efd3149a8c667
seyeon22 <sywolleych@gmail.com>
2024-07-18 20:10:07 +0900

    엔티티 생성 중

--- src/main/java/com/cafeattack/springboot/Domain/Entity/Cafe.java
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/Cafe.java
@@ -1,4 +1,46 @@
 package com.cafeattack.springboot.Domain.Entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Cafe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cafe_id;
+
+    @Column(nullable = false)
+    private String cafe_name;
+
+    @Column(nullable = false)
+    private BigDecimal latitude;
+
+    @Column(nullable = false)
+    private BigDecimal longitude;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String time;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private boolean heart;
+
+    @Column
+    private Integer review_cnt;  // 이게 필요할까...
+
+    @Column
+    private BigDecimal avg_score;
 }
--- src/main/java/com/cafeattack/springboot/Domain/Entity/Member.java
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/Member.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,7 +16,7 @@ import java.util.List;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int member_id;
+    private Integer member_id;
 
     @Column(nullable = false)
     private String name;
@@ -24,8 +25,32 @@ public class Member {
     private String nickname;
 
     @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = true)  // 약간 헷갈려
+    private String check_password;
 
     @Column(nullable = false)
     private Date birth;
+
+    @Column(nullable = false)
+    private boolean agreement;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<Bookmark> bookmarks = new ArrayList<>();
+
+    @Builder
+    public Member(String name, String nickname, String email, String password,
+                  String check_password, Date birth, boolean agreement) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.check_password = check_password;
+        this.birth = birth;
+        this.agreement = agreement;
+    }
 }
