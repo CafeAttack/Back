7e79f01bbd8a049a843321eee12288381977b02f
Aqoom01 <rlagusgh789@daum.net>
2024-11-05 14:39:15 +0900

    refactor: 엔티티 수정 완료

---
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/Tag.java
@@ -0,0 +1,27 @@
+package com.cafeattack.springboot.Domain.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int tagId;
+
+    @Column(nullable = false, length = 15)
+    private String tagName;
+
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL)
+    private List<Tag> tagList = new ArrayList<>();
+}
---
+++ src/main/java/com/cafeattack/springboot/Domain/Entity/mapping/ReviewTagsPK.java
@@ -0,0 +1,28 @@
+package com.cafeattack.springboot.Domain.Entity.mapping;
+
+import com.cafeattack.springboot.Domain.Entity.Review;
+import com.cafeattack.springboot.Domain.Entity.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewTagsPK {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
