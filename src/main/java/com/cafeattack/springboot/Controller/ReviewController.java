0d779f9e15e052ade7a61fdf3b362455954e6d7b
seyeon22 <sywolleych@gmail.com>
2024-12-20 16:06:05 +0900

    리뷰 작성 import 수정

--- src/main/java/com/cafeattack/springboot/Controller/ReviewController.java
+++ src/main/java/com/cafeattack/springboot/Controller/ReviewController.java
@@ -8,8 +8,6 @@ import com.cafeattack.springboot.Service.ReviewService;
 import com.cafeattack.springboot.common.BaseErrorResponse;
 import com.cafeattack.springboot.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
